a98a6c3a9ee6d03106651cf3702829decbb80c4a
edwardsbean <edwardsbean@gmail.com>
2014-11-25 17:34:06 +0800

    使用flock同步maven编译脚本

--- config-testframework/executor/package.sh
+++ config-testframework/executor/package.sh
@@ -3,7 +3,8 @@
 APP_HOME=$(dirname "$0")
 APP_HOME=$(cd "$APP_HOME"; pwd)
 DEST_HOME=$(cd $2; pwd)
-
+{
+flock -w 10 200
 if [ $# != 2 ]; then
     echo "参数不对"
     exit 1
@@ -14,3 +15,5 @@ cd $APP_HOME/thrift-package
 mvn package -q -Dmaven.test.skip=true
 mv $APP_HOME/thrift-package/target/thrift-package-1.0-SNAPSHOT.jar $DEST_HOME
 rm -rf $APP_HOME/thrift-package/src/main/java/*
+rm -rf $APP_HOME/thrift-package/target
+} 200<>$APP_HOME/.lock
